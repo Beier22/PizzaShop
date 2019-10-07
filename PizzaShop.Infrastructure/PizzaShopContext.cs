8582b6ece95ff7d6484970fccf9626b10f4d60f0
Mads <mads_beier@hotmail.com>
2019-10-07 11:59:42 +0200

    Added Context

---
+++ PizzaShop.Infrastructure/PizzaShopContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzaShop.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaShop.Infrastructure
+{
+    public class PizzaShopContext : DbContext
+    {
+        public PizzaShopContext(DbContextOptions<PizzaShopContext> opt) : base(opt) { }
+
+        public DbSet<FastFood> FastFoods { get; set; }
+        public DbSet<Pizza> Pizzas { get; set; }
+        public DbSet<Topping> Toppings { get; set; }
+    }
+}
