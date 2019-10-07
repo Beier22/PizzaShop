f8109fdd0459c2bd16791f0b1257e830eb91ce43
Mads <mads_beier@hotmail.com>
2019-10-07 14:05:27 +0200

    Create PizzasController.cs

---
+++ PizzaShop.UI.RestAPI/Controllers/PizzasController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaShop.UI.RestAPI.Controllers
+{
+    public class PizzasController : ControllerBase
+    {
+        // GET api/values
+        [Route("api/[controller]")]
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "hello", "hi" }; 
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
